e844a8736c8b0647f5174a143d00eed68e5f920e
daveg7lee <daveg7lee@gmail.com>
2021-01-14 19:49:57 +0900

    Goroutines

--- main.go
+++ main.go
@@ -1,43 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 func main() {
-	results := make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://nomadcoders.co/",
-	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go goodCount("dave")
+	goodCount("flynn")
 }
 
-var errRequestFailed = errors.New("Request Failed")
-
-func hitURL(url string) error {
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+func goodCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is Good", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
